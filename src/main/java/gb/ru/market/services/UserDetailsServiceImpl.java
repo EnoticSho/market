e3f362c30bffebe90baf37676a47595a06068630
EnoticSho <paradise666hell@mail.ru>
2022-12-28 14:51:09 +0300

    feat: updated

---
+++ src/main/java/gb/ru/market/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package gb.ru.market.services;
+
+import gb.ru.market.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException("User doesnt exists"));
+        return SecurityUser.fromUser(user);
+
+    }
+}
